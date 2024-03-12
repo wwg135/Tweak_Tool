0098de3fc6c3ee53a3c79eb09241d98b6dabbad3
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-03-12 17:45:10 +0800

    Update 一键备份和恢复工具.sh

--- 一键备份与恢复/一键备份和恢复工具.sh
+++ 一键备份与恢复/一键备份和恢复工具.sh
@@ -26,7 +26,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 	
 tweak2backup(){
-	debs="$(dpkg --get-selections | grep -v -E 'deinstall|gsc\.|cy\+|swift-|build-|llvm|clang' | grep -vw 'git' | grep -vwFf ./tweak_exclude_list | grep -v -w 'PreferenceLoader' | cut -f1 | awk '{print $1}')"
+	debs="$(dpkg --get-selections | grep -v -E 'deinstall|gsc\.|cy\+|swift-|build-|llvm|clang' | grep -vw 'git' | grep -vwFf ./tweak_exclude_list | cut -f1 | awk '{print $1}')"
    	for pkg in $debs; do
 		num=$(($num+1))
 		echo -e "${nco} 正在备份第"$num"个插件，请耐心等待...${nco}"
