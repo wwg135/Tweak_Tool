e873a4235264e7247d5a5f38bfdc7bdca5017089
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-03-12 17:56:24 +0800

    Update 权限修改.sh

--- 一键备份与恢复/权限修改.sh
+++ 一键备份与恢复/权限修改.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 PATH=/var/jb/bin:/var/jb/usr/bin:/var/jb/sbin:/var/jb/usr/sbin:$PATH
 
 echo -e "开始修改权限和所有权"
-
 chown mobile:mobile ./一键备份和恢复工具.sh
 chmod 755 ./一键备份和恢复工具.sh
-
 echo -e "权限和所有权修改成功"
 
 echo
